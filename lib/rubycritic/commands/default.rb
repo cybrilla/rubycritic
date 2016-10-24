d1b7c8c92b7e73e97ebaf61b2657109a3fd74b5b
Hemant Gowda <hemant.gowda@cybrilla.com>
2016-10-24 22:22:07 +0530

    Code cleanup
    * Code refactoring, DRY
    * Comparing between branches in local without pr id.

--- lib/rubycritic/commands/default.rb
+++ lib/rubycritic/commands/default.rb
@@ -17,8 +17,6 @@ module RubyCritic
         @base_branch_hash = {}
         @feature_branch_hash = {}
         @files_affected = []
-        @new_files = []
-        @deleted_files = []
         @analysed_modules
         @number
         @code_index_file_location
@@ -26,6 +24,7 @@ module RubyCritic
 
       def execute
         if Config.compare_between_branches  
+          Config.no_browser = true
           compare_branches
         else
           report(critique)
@@ -39,53 +38,49 @@ module RubyCritic
         switch_to_base_branch_and_compare
         switch_to_feature_branch_and_compare
         feature_branch_analysis
-        push_comments_to_gitlab
+        push_comments_to_gitlab if Config.merge_request_id
         compare_code_quality
       end
 
       def update_build_number
-        File.new('/tmp/build_count.txt', "a") unless (File.exist?('/tmp/build_count.txt'))
-        @number = File.open('/tmp/build_count.txt').readlines.first.to_i + 1 
-        File.write('/tmp/build_count.txt', @number)
+        build_file_location = '/tmp/build_count.txt'
+        File.new(build_file_location, "a") unless (File.exist?(build_file_location))
+        @number = File.open(build_file_location).readlines.first.to_i + 1 
+        File.write(build_file_location, @number)
       end
 
       def set_root_paths
-        Config.base_root_directory = Pathname.new("tmp/rubycritic/#{Config.base_branch}")
-        Config.feature_root_directory = Pathname.new("tmp/rubycritic/#{Config.feature_branch}")
-        Config.build_root_directory = Pathname.new("tmp/rubycritic/builds/build_#{@number}")
-        Config.no_browser = true
+        Config.base_root_directory = Pathname.new(base_root_directory)
+        Config.feature_root_directory = Pathname.new(feature_root_directory)
+        Config.build_root_directory = Pathname.new(build_directory)
       end
 
       def switch_to_base_branch_and_compare
-        `git checkout #{Config.base_branch}`
-        critic = critique('base_hash', true)
+        switch_branch(Config.base_branch)
+        critic = critique('base_branch_hash')
         Config.base_branch_score = critic.score
-        Config.root = "tmp/rubycritic/#{Config.base_branch}"
+        Config.root = base_root_directory
         Config.base_branch_flag = true
         report(critic)
         Config.base_branch_flag = false
       end
 
       def switch_to_feature_branch_and_compare
-        `git checkout #{Config.feature_branch}`
-        critic = critique('feature_hash', true)
+        switch_branch(Config.feature_branch)
+        critic = critique('feature_branch_hash')
         Config.feature_branch_score = critic.score
-        Config.root = "tmp/rubycritic/#{Config.feature_branch}"
+        Config.root = feature_root_directory
         Config.feature_branch_flag = true
         report(critic)
         Config.feature_branch_flag = false
-        `git checkout #{Config.base_branch}`
+        switch_branch(Config.base_branch)
       end
 
       def feature_branch_analysis
-        get_degraded_files
-        get_file_details
-        defected_modules = @analysed_modules.where(@files_affected)
-        paths = defected_modules.map { |mod| mod.path }
-        analysed_modules = AnalysedModulesCollection.new(paths, defected_modules)
-        Config.root = "tmp/rubycritic/builds/build_#{@number}"
-        Config.set_location = true
-        Config.build_flag = true
+        defected_modules = @analysed_modules.where(degraded_files)
+        analysed_modules = AnalysedModulesCollection.new(defected_modules.map(&:path), defected_modules)
+        Config.root = build_directory
+        Config.set_location = Config.build_flag = true
         @code_index_file_location = Reporter.generate_report(analysed_modules)
       end
 
@@ -94,8 +89,8 @@ module RubyCritic
         app_id = app_settings['app_id']
         secret = app_settings['secret']
         code_status = Config.base_branch_score > Config.feature_branch_score ? "> :negative_squared_cross_mark: **#{(Config.base_branch_score - Config.feature_branch_score).round(2)} \% Decreased** :thumbsdown: <br />" : "> :white_check_mark: **#{(Config.feature_branch_score - Config.base_branch_score).round(2)} \% Increased** :thumbsup: <br />" 
-        report = "<a href=#{@code_index_file_location} target='_blank'>View Report</a> ( " + @code_index_file_location + ' )'
-        note = URI::encode(code_status + "_#{Config.base_branch} score: #{Config.base_branch_score.round(2)}_ % <br />" + "_#{Config.feature_branch} score: #{Config.feature_branch_score.round(2)} %_ <br/>" + report )
+        # report = "<a href=#{@code_index_file_location} target='_blank'>View Report</a> ( " + @code_index_file_location + ' )'
+        note = URI::encode(code_status + "_#{Config.base_branch} score: #{Config.base_branch_score.round(2)}_ % <br />" + "_#{Config.feature_branch} score: #{Config.feature_branch_score.round(2)} %_ <br/>")
         HTTParty.post("https://vault.cybrilla.com/api/v3/projects/#{app_id}/merge_requests/#{Config.merge_request_id}/notes?body=#{note}",
         :headers => {'Private-Token' => secret} )
       end
@@ -107,48 +102,48 @@ module RubyCritic
         status_reporter
       end
 
+      def base_root_directory
+        "tmp/rubycritic/#{Config.base_branch}"
+      end
+
+      def feature_root_directory
+        "tmp/rubycritic/#{Config.feature_branch}"
+      end
+
+      def build_directory
+        "tmp/rubycritic/builds/build_#{@number}"
+      end
+
+      def switch_branch(branch)
+        `git checkout #{branch}`
+      end
+
       def build_details
         details = "Base branch (#{Config.base_branch}) score: " + Config.base_branch_score.to_s + "\n"
         details += "Feature branch (#{Config.feature_branch}) score: " + Config.feature_branch_score.to_s + "\n"
-        details += "#{@new_files.count} New Files Addded" + "\n"
-        details += "#{@deleted_files.count} Files Deleted" + "\n"
         details += status_reporter.status_message
         File.open("#{Config.build_root_directory}/build_details.txt", 'w') {|f| f.write(details) }
       end
 
-      def get_degraded_files
+      def degraded_files
         @feature_branch_hash.each do |k,v|
           @files_affected << k.to_s if !@base_branch_hash[k.to_sym].nil? && @base_branch_hash[k.to_sym] < v
         end
-      end
-
-      def get_file_details
-        if (@feature_branch_hash.size > @base_branch_hash.size)
-          difference = @feature_branch_hash.to_a - @base_branch_hash.to_a
-          @new_files = Hash[*difference.flatten].keys
-        else
-          difference = @base_branch_hash.to_a - @feature_branch_hash.to_a
-          @deleted_files = Hash[*difference.flatten].keys
-        end
-      end
-
-      def critique(cost_hash = nil, code_analysis = false)
-        analysed_modules = AnalysersRunner.new(paths).run
-        @analysed_modules = analysed_modules
-        build_cost_hash(cost_hash, analysed_modules) if code_analysis
-        RevisionComparator.new(paths).set_statuses(analysed_modules)
+        @files_affected
       end
 
       def build_cost_hash(cost_hash, analysed_modules)
-        complexity_hash = get_hash(cost_hash)
+        complexity_hash = eval("@#{cost_hash}")
         analysed_modules.each do |analysed_module|
           complexity_hash.merge!({ "#{analysed_module.name}": analysed_module.cost }) 
         end
       end
 
-      def get_hash(cost_hash)
-        return @base_branch_hash if cost_hash == 'base_hash'
-        return @feature_branch_hash if cost_hash == 'feature_hash'
+      def critique(cost_hash = nil)
+        analysed_modules = AnalysersRunner.new(paths).run
+        @analysed_modules = analysed_modules
+        build_cost_hash(cost_hash, analysed_modules) if cost_hash
+        RevisionComparator.new(paths).set_statuses(analysed_modules)
       end
 
       def report(analysed_modules)
